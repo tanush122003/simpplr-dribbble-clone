39026f4ba20278e030b4bdc7080a6dc5c374ab92
Tanush Chauhan <98251929+tanush122003@users.noreply.github.com>
2024-07-02 01:41:04 +0530

    Add files via upload

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -21,7 +21,7 @@ const ImageCard = ({ item }) => {
     <div class="card h-100">
       <img src={item.imageUrl} class="card-img-top" alt="..."/>
       <div id='mycard' class="card-body d-flex">
-        <img src="https://github.com/mdo.png" alt="mdo" width="25" height="25" class="rounded-circle"/>
+        <img src="https://github.com/mdo.png" alt="mdo" width="30" height="30" class="profile"/>
         <p class="card-title col-lg-5 px-2">{item.title}</p>
         <div id='likes' class="d-flex ms-lg-auto col-lg-3">
           <IoHeart onClick={handleLike} size="1.2em" className={`icon icon-like ${liked ? 'liked' : 'unliked'} col-md-3`}/>
