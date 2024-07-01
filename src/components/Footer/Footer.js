bc5b4ac21028377d2df3f038ab5d632695958568
Tanush Chauhan <98251929+tanush122003@users.noreply.github.com>
2024-07-02 01:27:41 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,11 +12,11 @@ import "../Footer/Footer.css"
 const Footer = ({ setFilter, setSearchQuery }) => {
   return (
 
-    <footer class="d-flex flex-wrap align-items-center py-3 my-4">
-    <div className='justify-content-between d-flex ms-lg-5'>
+    <footer class="d-flex flex-wrap align-items-center py-3">
+    <div className='justify-content-between d-flex ms-lg-4'>
     <div id='foot' class="col-sm-1 d-flex">
       <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-      <img id='logo1' className='logo' src={require("../images/logo.png")} alt="" />
+      <img id='logo1' src={require("../images/logo.png")} alt="" />
     </a>
     </div>
 
@@ -39,13 +39,10 @@ const Footer = ({ setFilter, setSearchQuery }) => {
     </ul>
     </div>
 
-    <div class="container">
+    <div id='lowfoot' class="container">
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
     <p class="col-md-4 mb-0 text-body-secondary">© 2024 Dribbble&nbsp;&nbsp;&nbsp;Terms&nbsp;&nbsp; Privacy&nbsp;&nbsp; Cookies</p>
-    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-    </a>
-
-    <ul class="nav col-md-4 justify-content-end">
+    <ul class="nav col-md-5 justify-content-end">
       <li class="nav-item"><a href="#" id='setme2' class="nav-link px-2">Jobs</a></li>
       <li class="nav-item"><a href="#" id='setme2' class="nav-link px-2">Designers</a></li>
       <li class="nav-item"><a href="#" id='setme2' class="nav-link px-2">Freelancers</a></li>
